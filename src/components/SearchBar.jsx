22febc18d755835e99bfe37361b1909e7f074eb3
Gunjan Agrawal <99130166+gunjan192002@users.noreply.github.com>
2024-06-14 10:13:00 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaFilter } from 'react-icons/fa';
 
 const SearchBar = ({ problems, onSearch }) => {
     const [searchTerm, setSearchTerm] = useState('');
@@ -7,8 +7,10 @@ const SearchBar = ({ problems, onSearch }) => {
     const [selectedSolved, setSelectedSolved] = useState('');
     const [difficultyDropdownOpen, setDifficultyDropdownOpen] = useState(false);
     const [solvedDropdownOpen, setSolvedDropdownOpen] = useState(false);
+    const [filterOpen, setFilterOpen] = useState(false);
     const difficultyRef = useRef(null);
     const solvedRef = useRef(null);
+    const filterRef = useRef(null);
 
     const handleInputChange = (event) => {
         setSearchTerm(event.target.value);
@@ -44,6 +46,9 @@ const SearchBar = ({ problems, onSearch }) => {
             if (solvedRef.current && !solvedRef.current.contains(event.target)) {
                 setSolvedDropdownOpen(false);
             }
+            if (filterRef.current && !filterRef.current.contains(event.target)) {
+                setFilterOpen(false);
+            }
         };
         document.addEventListener('mousedown', handleClickOutside);
         return () => {
@@ -53,7 +58,7 @@ const SearchBar = ({ problems, onSearch }) => {
 
     return (
         <div className="flex justify-center mt-6">
-            <div className="flex items-center bg-white rounded-md shadow-md p-2" style={{ minWidth: "600px" }}>
+            <div className="flex items-center bg-white rounded-md shadow-md p-2 w-full max-w-2xl">
                 <FaSearch className="ml-3 text-gray-500" />
                 <input
                     type="text"
@@ -62,7 +67,66 @@ const SearchBar = ({ problems, onSearch }) => {
                     onChange={handleInputChange}
                     className="flex-grow px-4 py-2 border-transparent rounded-full focus:outline-none"
                 />
-                <div className="flex">
+                <div className="flex md:hidden ml-2 relative">
+                    <button
+                        onClick={() => setFilterOpen(!filterOpen)}
+                        className="py-2 px-2 text-white bg-blue-600 rounded-md focus:outline-none"
+                    >
+                        <FaFilter />
+                    </button>
+                    {filterOpen && (
+                        <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-lg z-10" ref={filterRef}>
+                            <div className="px-4 py-2">
+                             
+                                <button
+                                    onClick={() => handleDifficultyChange("HARD")}
+                                    className="block w-full py-2 px-2 text-left transition duration-300 delay-100 hover:bg-gray-200 text-sm"
+                                >
+                                    HARD
+                                </button>
+                                <button
+                                    onClick={() => handleDifficultyChange("MEDIUM")}
+                                    className="block w-full py-2 px-2 text-left transition duration-300 delay-100 hover:bg-gray-200 text-sm"
+                                >
+                                    MEDIUM
+                                </button>
+                                <button
+                                    onClick={() => handleDifficultyChange("EASY")}
+                                    className="block w-full py-2 px-2 text-left transition duration-300 delay-100 hover:bg-gray-200 text-sm"
+                                >
+                                    EASY
+                                </button>
+                                <button
+                                    onClick={() => handleDifficultyChange("")}
+                                    className="block w-full py-2 px-2 text-left transition duration-300 delay-100 hover:bg-gray-200 text-sm"
+                                >
+                                    ALL Difficulty
+                                </button>
+                                <button
+                                    onClick={() => handleSolvedChange("SOLVED")}
+                                    className="block w-full py-2 px-2 text-left transition duration-300 delay-100 hover:bg-gray-200 text-sm"
+                                >
+                                    SOLVED
+                                </button>
+                                <button
+                                    onClick={() => handleSolvedChange("UNSOLVED")}
+                                    className="block w-full py-2 px-2 text-left transition duration-300 delay-100 hover:bg-gray-200 text-sm"
+                                >
+                                    UNSOLVED
+                                </button>
+                                <button
+                                    onClick={() => handleSolvedChange("")}
+                                    className="block w-full py-2 px-2 text-left transition duration-300 delay-100 hover:bg-gray-200 text-sm"
+                                >
+                                    SOlVED & UNSOLVED
+                                </button>
+                            </div>
+                            
+                        </div>
+                    )}
+                </div>
+                {/* <div className={` hidden md-flex   items-center md:block`}> */}
+                <div className={`md:block  md:flex hidden`}>
                     <div className="relative mx-2">
                         <button
                             onClick={() => setDifficultyDropdownOpen(!difficultyDropdownOpen)}
