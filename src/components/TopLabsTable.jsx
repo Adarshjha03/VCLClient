ac3aa6961b1cee2cb204a00f910d176d2cca786b
Adarshjha03 <jha.adarshk03@gmail.com>
2024-06-26 22:56:31 +0530

    dasboard

--- src/components/TopLabsTable.jsx
+++ src/components/TopLabsTable.jsx
@@ -1,27 +1,25 @@
-import React from "react";
+import React from 'react';
 
 const TopLabsTable = ({ data }) => {
     return (
-        <div className="h-full flex items-center justify-center">
-            <table className="min-w-full bg-white border border-gray-200 rounded-lg">
-                <thead>
-                    <tr>
-                        <th className="py-2 px-4 border-b border-gray-200">Category</th>
-                        <th className="py-2 px-4 border-b border-gray-200">Lab Most Solved</th>
-                        <th className="py-2 px-4 border-b border-gray-200">Num Solves</th>
+        <table className="w-full table-auto bg-transparent">
+            <thead>
+                <tr>
+                    <th className=" border-y px-4 py-2">Category</th>
+                    <th className="border-y px-4 py-2 text-center">Lab Most Solved</th>
+                    <th className="border-y px-4 py-2 text-right">Num Solves</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((lab, index) => (
+                    <tr key={index}>
+                        <td className="border-y px-4 py-2">{lab.category}</td>
+                        <td className="border-y px-4 py-2 text-center">{lab.top_lab.lab_name}</td>
+                        <td className="border-y px-4 py-2 text-right">{lab.top_lab.num_solves}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    {data.map((item, index) => (
-                        <tr key={index}>
-                            <td className="py-2 px-4 border-b border-gray-200">{item.category}</td>
-                            <td className="py-2 px-4 border-b border-gray-200">{item.top_lab ? item.top_lab.lab_name : "N/A"}</td>
-                            <td className="py-2 px-4 border-b border-gray-200">{item.top_lab ? item.top_lab.num_solves : "N/A"}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                ))}
+            </tbody>
+        </table>
     );
 };
 
